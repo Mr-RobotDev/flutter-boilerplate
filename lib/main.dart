1a73c29c84c4197c8b34196fc04a0c312f856cfa
Abdullah1071 <abdullahshahid1071@gmail.com>
2023-03-12 01:45:24 +0500

    update: use simple theme instead of flex color theme and system font instead of google font

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:boiler_plate/app/app.locator.dart';
 import 'package:boiler_plate/ui/setup/setup_bottom_sheet_ui.dart';
 import 'package:boiler_plate/ui/setup/setup_dialog_ui.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 import 'app/app.router.dart';
@@ -23,51 +21,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: FlexThemeData.light(
-        colors: const FlexSchemeColor(
-          primary: Color(0xff004881),
-          primaryContainer: Color(0xffd0e4ff),
-          secondary: Color(0xffac3306),
-          secondaryContainer: Color(0xffffdbcf),
-          tertiary: Color(0xff006875),
-          tertiaryContainer: Color(0xff95f0ff),
-          appBarColor: Color(0xffffdbcf),
-          error: Color(0xffb00020),
-        ),
-        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-        blendLevel: 20,
-        appBarOpacity: 0.95,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 20,
-          blendOnColors: false,
-          elevatedButtonRadius: 32.0,
-          inputDecoratorRadius: 40.0,
-        ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        fontFamily: GoogleFonts.poppins().fontFamily,
+      theme: ThemeData.light(
+        useMaterial3: true,
       ),
-      darkTheme: FlexThemeData.dark(
-        colors: const FlexSchemeColor(
-          primary: Color(0xff9fc9ff),
-          primaryContainer: Color(0xff00325b),
-          secondary: Color(0xffffb59d),
-          secondaryContainer: Color(0xff872100),
-          tertiary: Color(0xff86d2e1),
-          tertiaryContainer: Color(0xff004e59),
-          appBarColor: Color(0xff872100),
-          error: Color(0xffcf6679),
-        ),
-        surfaceMode: FlexSurfaceMode.highScaffoldLowSurface,
-        blendLevel: 15,
-        appBarStyle: FlexAppBarStyle.background,
-        appBarOpacity: 0.90,
-        subThemesData: const FlexSubThemesData(
-          blendOnLevel: 30,
-          elevatedButtonRadius: 32.0,
-          inputDecoratorRadius: 40.0,
-        ),
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        fontFamily: GoogleFonts.poppins().fontFamily,
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
       ),
       themeMode: ThemeMode.system,
       initialRoute: Routes.startupView,
